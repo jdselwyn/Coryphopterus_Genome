cee4147e71d8495dbacad2a8533a25bc9ee7d4ce
jdselwyn <jdselwyn@gmail.com>
2021-10-29 13:08:29 -0500

    Update buildTree.R

--- scripts/buildTree.R
+++ scripts/buildTree.R
@@ -1,7 +1,7 @@
 args <- commandArgs(trailingOnly=TRUE)
 aligned_fasta <- args[1]
 out_prefix <- args[2]
-NBOOT <- args[3]
+NBOOT <- as.integer(args[3])
 
 ##TODO - right now assume's GTR+G+I is going to be best model - sort out way to fill in settings based on what the nucleic model test finds
 
@@ -53,30 +53,30 @@ dev.off()
 write.tree(midpoint(fish_ml$tree), file = str_c(out_prefix, '_ml.tre'))
 write.tree(fish_bs, file = str_c(out_prefix, '_bootstrap', NBOOT, '.png'))
 
-library(ggraph)
-library(tidygraph)
-
-midpoint(fish_ml$tree) %>%
-  as_tbl_graph(directed = TRUE) %>%
-  mutate(name = if_else(str_detect(name, 'Node'), '', name)) %>%
-  # mutate(distance_to_cope = node_distance_to(name %in% c('Gobiiformes', 'Gobiidae', 'Coryphopterus', 'Coryphopterus hyalinus'), 
-  #                                            mode = 'all')) %>%
-  create_layout(layout='dendrogram') %>%
-  ggraph() +
-  geom_edge_elbow(show.legend = FALSE) +
-  geom_node_text(aes(label = name),size=4, 
-                 show.legend = TRUE, nudge_y = 0, hjust=1) +
-  # labs(colour = 'Distance to Coryphopterus') +
-  scale_y_reverse() +
-  coord_flip() +
-  theme_void() +
-  theme(legend.position = 'bottom')
-
-fish_ml_store <- fish_ml
-fish_ml$tree$node.label
-
-fish_ml$tree$Nnode
-fish_bs[[1]]
+# library(ggraph)
+# library(tidygraph)
+# 
+# midpoint(fish_ml$tree) %>%
+#   as_tbl_graph(directed = TRUE) %>%
+#   mutate(name = if_else(str_detect(name, 'Node'), '', name)) %>%
+#   # mutate(distance_to_cope = node_distance_to(name %in% c('Gobiiformes', 'Gobiidae', 'Coryphopterus', 'Coryphopterus hyalinus'), 
+#   #                                            mode = 'all')) %>%
+#   create_layout(layout='dendrogram') %>%
+#   ggraph() +
+#   geom_edge_elbow(show.legend = FALSE) +
+#   geom_node_text(aes(label = name),size=4, 
+#                  show.legend = TRUE, nudge_y = 0, hjust=1) +
+#   # labs(colour = 'Distance to Coryphopterus') +
+#   scale_y_reverse() +
+#   coord_flip() +
+#   theme_void() +
+#   theme(legend.position = 'bottom')
+# 
+# fish_ml_store <- fish_ml
+# fish_ml$tree$node.label
+# 
+# fish_ml$tree$Nnode
+# fish_bs[[1]]
 
 tree$node.label <- c("", round(runif(17-1), 3))
 
